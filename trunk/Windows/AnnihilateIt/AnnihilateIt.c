dd9f92ff88414765c96652eca0612d0bc2fda45e
Mikalai <Mikalai@0b952dd9-c8a5-684b-ab63-ecb492eb956f>
2011-03-13 04:48:56 +0000

    - created ignore list - add unit test calls
    git-svn-id: https://pl3.projectlocker.com/nexuzzz/ait/svn@9 0b952dd9-c8a5-684b-ab63-ecb492eb956f

--- trunk/Windows/AnnihilateIt/AnnihilateIt.c
+++ trunk/Windows/AnnihilateIt/AnnihilateIt.c
@@ -2,9 +2,30 @@
 //
 
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include "Common\MsAnimation.h"
+#include "Common\MsAnimationBundle.h"
+#include "Common\MsBox.h"
+#include "Common\MsBoxGrid.h"
+#include "Common\MsCommon.h"
+#include "Common\MsGrid.h"
+#include "Common\MsList.h"
+#include "Common\MsPalette.h"
+
 
 
 int main(int argc, char* argv[])
 {
+	ms_animation_unit_test();
+	ms_animationbundle_unit_test();
+	ms_box_unit_test();
+	ms_boxgrid_unit_test();
+	ms_list_unit_test();
+
+	_CrtDumpMemoryLeaks();
+
 	return 0;
 }
