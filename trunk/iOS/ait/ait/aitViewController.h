07875dae238566cb448d2988f18060e7771bbd8a
Mikalai (mac) <Mikalai (mac)@0b952dd9-c8a5-684b-ab63-ecb492eb956f>
2011-04-21 04:16:42 +0000

    "Importing ait"
    git-svn-id: https://pl3.projectlocker.com/nexuzzz/ait/svn@84 0b952dd9-c8a5-684b-ab63-ecb492eb956f

---
+++ trunk/iOS/ait/ait/aitViewController.h
@@ -0,0 +1,34 @@
+//
+//  aitViewController.h
+//  ait
+//
+//  Created by Mikalai Silivonik on 11-04-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <OpenGLES/EAGL.h>
+
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+
+@interface aitViewController : UIViewController {
+@private
+    EAGLContext *context;
+    GLuint program;
+    
+    BOOL animating;
+    NSInteger animationFrameInterval;
+    CADisplayLink *displayLink;
+}
+
+@property (readonly, nonatomic, getter=isAnimating) BOOL animating;
+@property (nonatomic) NSInteger animationFrameInterval;
+
+- (void)startAnimation;
+- (void)stopAnimation;
+
+@end
---
+++ trunk/iOS/ait/aitTests/aitTests.m
@@ -0,0 +1,33 @@
+//
+//  aitTests.m
+//  aitTests
+//
+//  Created by Mikalai Silivonik on 11-04-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "aitTests.h"
+
+
+@implementation aitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in aitTests");
+}
+
+@end
