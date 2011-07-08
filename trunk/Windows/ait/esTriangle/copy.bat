9f27a27c01d1b4ac948bc6d161d166c9f9aa262c
msilivonik@gmail.com <msilivonik@gmail.com@0b952dd9-c8a5-684b-ab63-ecb492eb956f>
2011-07-08 16:57:04 +0000

    fixed copying command
    git-svn-id: https://pl3.projectlocker.com/nexuzzz/ait/svn@190 0b952dd9-c8a5-684b-ab63-ecb492eb956f

--- trunk/Windows/ait/esTriangle/copy.bat
+++ trunk/Windows/ait/esTriangle/copy.bat
@@ -1,1 +1,1 @@
-xcopy "C:\Program Files (x86)\NVIDIA Corporation\win_x86_es2emu\ait\ms\Annihilate\data" "C:\Program Files (x86)\NVIDIA Corporation\win_x86_es2emu\ait\esTriangle\Debug\data\" /E
+xcopy "C:\Program Files (x86)\NVIDIA Corporation\win_x86_es2emu\ait\ms\Annihilate\data" "C:\Program Files (x86)\NVIDIA Corporation\win_x86_es2emu\ait\esTriangle\Debug\data\" /E /Y
