b9335404a7210e7359d2a3188b88d8843becfb06
msilivonik@gmail.com <msilivonik@gmail.com@0b952dd9-c8a5-684b-ab63-ecb492eb956f>
2011-07-12 05:07:18 +0000

    forget to check in file in last revision
    git-svn-id: https://pl3.projectlocker.com/nexuzzz/ait/svn@195 0b952dd9-c8a5-684b-ab63-ecb492eb956f

--- trunk/Common/ms/Annihilate/msBoxGridRenderer.h
+++ trunk/Common/ms/Annihilate/msBoxGridRenderer.h
@@ -56,7 +56,7 @@ class msBoxGridRenderer
 
     void drawExplosions();
 
-	void drawBox(msShaderProgram *m_program, msPalette *palette, msBox *box, msColor *c);
+	void drawBox(msShaderProgram *m_program, msPalette *palette, msBox *box, msColor *c, int mode);
 
 
     void drawBoxesWithShockWave(msBoxGrid *boxGrid);
