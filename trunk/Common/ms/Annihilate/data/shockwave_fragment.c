2686367d988e144f857dfab733ad1c0f2dc7499a
msilivonik@gmail.com <msilivonik@gmail.com@0b952dd9-c8a5-684b-ab63-ecb492eb956f>
2011-07-12 19:38:30 +0000

    more lighter wave
    git-svn-id: https://pl3.projectlocker.com/nexuzzz/ait/svn@198 0b952dd9-c8a5-684b-ab63-ecb492eb956f

--- trunk/Common/ms/Annihilate/data/shockwave_fragment.c
+++ trunk/Common/ms/Annihilate/data/shockwave_fragment.c
@@ -11,9 +11,12 @@ varying vec4 texcoord0;
 
 void main()
 {
+	vec4 color;
+
 	vec3 shockParams = vec3(10.0, 0.2, 0.1 * power);	
 	vec2 uv = texcoord0.xy;	
-	vec2 texCoord = uv;	
+	vec2 texCoord = uv;
+	
 	float distance = distance(uv, location);
 	float diff = (distance - radius);
 	if (diff <= shockParams.z && diff >= -shockParams.z)
@@ -22,8 +25,14 @@ void main()
 		float diffRadius = diff  * powDiff;
 		vec2 diffUV = normalize(uv - location);
 		texCoord = uv + (diffUV * diffRadius);
+		 
+		color = texture2D(tex, texCoord);
+		color += (diffRadius * 2.5);
 	}
-
+	else
+	{
+		color = texture2D(tex, texCoord);
+	}	
 	
-	gl_FragColor = texture2D(tex, texCoord);
+	gl_FragColor = color;
 }
